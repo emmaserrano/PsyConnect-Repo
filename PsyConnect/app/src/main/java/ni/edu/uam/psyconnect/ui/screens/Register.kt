e7d9bb324be49f13398f57511417351def4b4bab
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-06-01 15:37:34 -0600

    feat(register): crear pantalla de registro

--- PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/ui/screens/Register.kt
+++ PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/ui/screens/Register.kt
@@ -1,2 +1,15 @@
 package ni.edu.uam.psyconnect.ui.screens
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ni.edu.uam.psyconnect.R
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Conecta con el XML de registro
+        setContentView(R.layout.activity_register)
+    }
+}
