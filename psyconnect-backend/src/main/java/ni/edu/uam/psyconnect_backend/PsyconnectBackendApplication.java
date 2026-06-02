f3e803f3c4b44269f384f8d1809c94c1f1924bc0
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-02 17:05:09 -0600

    chore: generar proyecto base de Spring Boot

---
+++ psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/PsyconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.psyconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyconnectBackendApplication.class, args);
+	}
+
+}
