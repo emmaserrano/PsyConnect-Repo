81e63c39f4fe5b8a98202a3cc29aab1f387a84f8
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-02 17:15:03 -0600

    feat: agregar endpoint de registro de usuarios

---
+++ psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/controller/UserController.java
@@ -0,0 +1,22 @@
+package ni.edu.uam.psyconnect_backend.controller;
+
+import ni.edu.uam.psyconnect_backend.model.User;
+import ni.edu.uam.psyconnect_backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+
+        return userService.saveUser(user);
+    }
+}
