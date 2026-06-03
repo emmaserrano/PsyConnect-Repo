3d4ea12c1c7b2a80f663f2e98985b3f911c867d1
jeyni02 <jeynilomary@uam.edu.ni>
2026-06-03 15:16:12 -0600

    feat: create test result entity

---
+++ psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/model/TestResult.java
@@ -0,0 +1,73 @@
+package ni.edu.uam.psyconnect_backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "test_results")
+public class TestResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Integer percentage;
+
+    private String level;
+
+    private LocalDateTime createdAt;
+
+    public TestResult() {
+    }
+
+    public TestResult(
+            Long userId,
+            Integer percentage,
+            String level,
+            LocalDateTime createdAt
+    ) {
+        this.userId = userId;
+        this.percentage = percentage;
+        this.level = level;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Integer getPercentage() {
+        return percentage;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setPercentage(Integer percentage) {
+        this.percentage = percentage;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
