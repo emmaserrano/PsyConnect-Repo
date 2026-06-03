fd5d83fa0f09a76a7c9adaef539160124c7592f0
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-03 13:03:59 -0600

    feat(auth): agregar verificación de sesión automática en MainActivity

--- PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/MainActivity.kt
+++ PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/MainActivity.kt
@@ -3,19 +3,29 @@ package ni.edu.uam.psyconnect
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ni.edu.uam.psyconnect.ui.screens.Home
 import ni.edu.uam.psyconnect.ui.screens.Login
-import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Navega automáticamente al Login
-        val intent = Intent(this, Login::class.java)
-        startActivity(intent)
+        val sharedPreferences = getSharedPreferences("psyconnect", MODE_PRIVATE)
 
-        // Cierra MainActivity
+        val isLogged = sharedPreferences.getBoolean("isLogged", false)
+
+        if (isLogged) {
+
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+
+        } else {
+
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
         finish()
     }
 }
