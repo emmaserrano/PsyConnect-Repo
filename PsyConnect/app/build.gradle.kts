3926d1408fd2f2594fc6fccb10dbdead05069cad
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-06-04 12:42:42 -0600

    feat: implementa navegación inferior y simplifica pantalla principal

--- PsyConnect/app/build.gradle.kts
+++ PsyConnect/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("com.github.PhilJay:MPAndroidChart:3.1.0")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
