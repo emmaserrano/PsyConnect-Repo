419e150584c930a05f4b79fbbe67c4c1f9a51159
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-12 19:59:12 -0600

    feat: agregar Glide para cargar imágenes en tarjetas de psicólogos

--- PsyConnect/app/build.gradle.kts
+++ PsyConnect/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:3.1.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation(libs.google.material)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
