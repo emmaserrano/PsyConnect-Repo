c951c071b589355a35522fd1a54500cae100efeb
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-12 20:17:09 -0600

    feat: agregar modelo completo de psicologos

--- psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/controller/PsychologistController.java
+++ psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/controller/PsychologistController.java
@@ -40,4 +40,6 @@ public class PsychologistController {
         return service.save(psychologist);
     }
 
+
+
 }
--- psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/model/Psychologist.java
+++ psyconnect-backend/src/main/java/ni/edu/uam/psyconnect_backend/model/Psychologist.java
@@ -1,7 +1,5 @@
 package ni.edu.uam.psyconnect_backend.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
 
 @Entity
@@ -35,8 +33,133 @@ public class Psychologist {
     public Psychologist() {
     }
 
-    // constructor
-    // getters
-    // setters
+    public Psychologist(
+            Long id,
+            String name,
+            String specialty,
+            String phone,
+            String city,
+            String email,
+            String description,
+            String photoUrl,
+            Boolean virtualAttention,
+            Double rating,
+            Boolean featured
+    ) {
+        this.id = id;
+        this.name = name;
+        this.specialty = specialty;
+        this.phone = phone;
+        this.city = city;
+        this.email = email;
+        this.description = description;
+        this.photoUrl = photoUrl;
+        this.virtualAttention = virtualAttention;
+        this.rating = rating;
+        this.featured = featured;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(
+            String name
+    ) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(
+            String specialty
+    ) {
+        this.specialty = specialty;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(
+            String phone
+    ) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(
+            String city
+    ) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(
+            String email
+    ) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(
+            String description
+    ) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(
+            String photoUrl
+    ) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Boolean getVirtualAttention() {
+        return virtualAttention;
+    }
+
+    public void setVirtualAttention(
+            Boolean virtualAttention
+    ) {
+        this.virtualAttention = virtualAttention;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(
+            Double rating
+    ) {
+        this.rating = rating;
+    }
+
+    public Boolean getFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(
+            Boolean featured
+    ) {
+        this.featured = featured;
+    }
 }
