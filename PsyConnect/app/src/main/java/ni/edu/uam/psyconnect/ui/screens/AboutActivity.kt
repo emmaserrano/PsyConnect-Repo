5ffc7cc1f3f8dfedc5154cdf0d660f706cb00b2c
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-06-20 12:32:23 -0600

    feat(about): implementar pantalla Acerca de PsyConnect

--- PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/ui/screens/AboutActivity.kt
+++ PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/ui/screens/AboutActivity.kt
@@ -1,14 +1,27 @@
 package ni.edu.uam.psyconnect.ui.screens
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ni.edu.uam.psyconnect.R
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_about)
+
+        val btnBack =
+            findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+
+            finish()
+
+        }
+
     }
 
 }
