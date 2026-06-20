6221d539180878cc8fb1d7977211956c2ddba662
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-06-20 11:23:17 -0600

    feat(settings): crear pantalla de ajustes

---
+++ PsyConnect/app/src/main/java/ni/edu/uam/psyconnect/ui/screens/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package ni.edu.uam.psyconnect.ui.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ni.edu.uam.psyconnect.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
